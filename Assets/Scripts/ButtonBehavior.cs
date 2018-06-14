ece798fb81ac6921f0baaccdef5d18188641108c
blueshinymarble <blueshinymarble@gmail.com>
2018-06-14 21:54:31 +0900

    added soldier and ship but ship model is rotated wrong

--- Assets/Scripts/ButtonBehavior.cs
+++ Assets/Scripts/ButtonBehavior.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 public class ButtonBehavior : MonoBehaviour
 {
 
-    public bool turretIsSelected = false;
+    public bool turretSelected = false;
     public bool powerPlantSelected = false;
     public bool aAGunSelected = false;
     public bool terraformerSelected = false;
+    public bool soldierSelected = false;
+    public bool shipSelected;
     private Image myAaGunImage;
     private Image myTurretImage;
     private Image myPowerPlantImage;
     private Image myTerraformerImage;
+    private Image mySoldierImage;
+    private Image myShipImage;
     private Board myBoard;
-    private Panel panel;
 
 	// Use this for initialization
 	void Start ()
@@ -25,7 +28,8 @@ public class ButtonBehavior : MonoBehaviour
         myPowerPlantImage = GameObject.Find("Power Plant Button").GetComponent<Image>();
         myAaGunImage = GameObject.Find("AA Gun Button").GetComponent<Image>();
         myTerraformerImage = GameObject.Find("Terraformer Button").GetComponent<Image>();
-        panel = GameObject.Find("Panel").GetComponent<Panel>();
+        mySoldierImage = GameObject.Find("Soldier Button").GetComponent<Image>();
+        myShipImage = GameObject.Find("Ship Button").GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
@@ -41,10 +45,10 @@ public class ButtonBehavior : MonoBehaviour
 
     public void TestTurretButton()
     {
-        if (!turretIsSelected)
+        if (!turretSelected)
         {
             GameObject.Find("Turret Button").tag = "Clicked Button";
-            turretIsSelected = true;
+            turretSelected = true;
             myTurretImage.color = Color.green;
             Button[] unclickedButtons = FindObjectsOfType<Button>();
             foreach (Button button in unclickedButtons)
@@ -59,7 +63,7 @@ public class ButtonBehavior : MonoBehaviour
         {
             Button[] allButtons = FindObjectsOfType<Button>(); 
             myTurretImage.color = Color.white;
-            turretIsSelected = false;
+            turretSelected = false;
             foreach (Button button in allButtons)
             {
                 button.interactable = true;
@@ -154,5 +158,64 @@ public class ButtonBehavior : MonoBehaviour
                 button.tag = "Unclicked Button";
             }
         }
+    }
+
+    public void TestSoldier()
+    {
+        if (!turretSelected)
+        {
+            GameObject.Find("Soldier Button").tag = "Clicked Button";
+            soldierSelected = true;
+            mySoldierImage.color = Color.green;
+            Button[] unclickedButtons = FindObjectsOfType<Button>();
+            foreach (Button button in unclickedButtons)
+            {
+                if (button.tag == "Unclicked Button")
+                {
+                    button.interactable = false;
+                }
+            }
+        }
+        else
+        {
+            Button[] allButtons = FindObjectsOfType<Button>();
+            mySoldierImage.color = Color.white;
+            soldierSelected = false;
+            foreach (Button button in allButtons)
+            {
+                button.interactable = true;
+                button.tag = "Unclicked Button";
+            }
+        }
+    }
+
+    public void TestShip()
+    {
+        if (!turretSelected)
+        {
+            GameObject.Find("Ship Button").tag = "Clicked Button";
+            shipSelected = true;
+            myShipImage.color = Color.green;
+            Button[] unclickedButtons = FindObjectsOfType<Button>();
+            foreach (Button button in unclickedButtons)
+            {
+                if (button.tag == "Unclicked Button")
+                {
+                    button.interactable = false;
+                }
+            }
+        }
+        else
+        {
+            Button[] allButtons = FindObjectsOfType<Button>();
+            myShipImage.color = Color.white;
+            shipSelected = false;
+            foreach (Button button in allButtons)
+            {
+                button.interactable = true;
+                button.tag = "Unclicked Button";
+            }
+        }
+
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -9,6 +9,9 @@ public class Tile : MonoBehaviour
     public GameObject turret;
     public GameObject powerPlant;
     public GameObject aAGun;
+    public GameObject terraformer;
+    public GameObject soldier;
+    public GameObject ship;
     public GameObject[] terrainTiles;
 
     private ButtonBehavior buttonBehavior;
@@ -35,7 +38,7 @@ public class Tile : MonoBehaviour
 
     public void OnMouseDown()
     {
-        if (buttonBehavior.turretIsSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
+        if (buttonBehavior.turretSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
         {
             GameObject spawnedTurret = Instantiate(turret, transform.position, Quaternion.identity);
             spawnedTurret.transform.parent = gameObject.transform;
@@ -47,14 +50,33 @@ public class Tile : MonoBehaviour
             spawnedPowerPlant.transform.parent = gameObject.transform;
             spawnedPowerPlant.transform.rotation = gameObject.transform.rotation;
         }
-
         else if (buttonBehavior.aAGunSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
         {
             GameObject spawnedAAGun = Instantiate(aAGun, transform.position, Quaternion.identity);
             spawnedAAGun.transform.parent = gameObject.transform;
             spawnedAAGun.transform.rotation = gameObject.transform.rotation;
         }
+        else if (buttonBehavior.terraformerSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
+        {
+            GameObject spawnedTerraformer = Instantiate(terraformer, transform.position, Quaternion.identity);
+            spawnedTerraformer.transform.parent = gameObject.transform;
+            spawnedTerraformer.transform.rotation = gameObject.transform.rotation;
+        }
+        else if (buttonBehavior.soldierSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
+        {
+            GameObject spawnedSoldier = Instantiate(soldier, transform.position, Quaternion.identity);
+            spawnedSoldier.transform.parent = gameObject.transform;
+            spawnedSoldier.transform.rotation = gameObject.transform.rotation;
+        }
+        else if (buttonBehavior.shipSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
+        {
+            GameObject spawnedShip = Instantiate(ship, transform.position, Quaternion.identity);
+            spawnedShip.transform.parent = gameObject.transform;
+            spawnedShip.transform.rotation = gameObject.transform.rotation;
+        }
+
+
     }
 
-    
+
 }
