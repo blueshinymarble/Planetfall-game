cdd70654fe460a98b6f1aa6239b7e19e0f87fd62
blueshinymarble <blueshinymarble@gmail.com>
2018-06-17 09:24:26 +0900

    made the ui panel properly block mouse clicks on both ui and game world

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.EventSystems;
 
 public class Tile : MonoBehaviour
 {
@@ -28,6 +29,7 @@ public class Tile : MonoBehaviour
         {
             board.PopulateBoard((Board.TerrainTilesEnum)Random.Range(0, 6), transform);
         }
+       
     }
 
     // Update is called once per frame
@@ -38,42 +40,44 @@ public class Tile : MonoBehaviour
 
     public void OnMouseDown()
     {
-        if (buttonBehavior.turretSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
+        if (!EventSystem.current.IsPointerOverGameObject())
         {
-            GameObject spawnedTurret = Instantiate(turret, transform.position, Quaternion.identity);
-            spawnedTurret.transform.parent = gameObject.transform;
-            spawnedTurret.transform.rotation = gameObject.transform.rotation;
+            if (buttonBehavior.turretSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
+            {
+                GameObject spawnedTurret = Instantiate(turret, transform.position, Quaternion.identity);
+                spawnedTurret.transform.parent = gameObject.transform;
+                spawnedTurret.transform.rotation = gameObject.transform.rotation;
+            }
+            else if (buttonBehavior.powerPlantSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
+            {
+                GameObject spawnedPowerPlant = Instantiate(powerPlant, transform.position, Quaternion.identity);
+                spawnedPowerPlant.transform.parent = gameObject.transform;
+                spawnedPowerPlant.transform.rotation = gameObject.transform.rotation;
+            }
+            else if (buttonBehavior.aAGunSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
+            {
+                GameObject spawnedAAGun = Instantiate(aAGun, transform.position, Quaternion.identity);
+                spawnedAAGun.transform.parent = gameObject.transform;
+                spawnedAAGun.transform.rotation = gameObject.transform.rotation;
+            }
+            else if (buttonBehavior.terraformerSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
+            {
+                GameObject spawnedTerraformer = Instantiate(terraformer, transform.position, Quaternion.identity);
+                spawnedTerraformer.transform.parent = gameObject.transform;
+                spawnedTerraformer.transform.rotation = gameObject.transform.rotation;
+            }
+            else if (buttonBehavior.soldierSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
+            {
+                GameObject spawnedSoldier = Instantiate(soldier, transform.position, Quaternion.identity);
+                spawnedSoldier.transform.parent = gameObject.transform;
+                spawnedSoldier.transform.rotation = gameObject.transform.rotation;
+            }
+            else if (buttonBehavior.shipSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
+            {
+                GameObject spawnedShip = Instantiate(ship, transform.position, Quaternion.identity);
+                spawnedShip.transform.parent = gameObject.transform;
+            }
         }
-        else if (buttonBehavior.powerPlantSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
-        {
-            GameObject spawnedPowerPlant = Instantiate(powerPlant, transform.position, Quaternion.identity);
-            spawnedPowerPlant.transform.parent = gameObject.transform;
-            spawnedPowerPlant.transform.rotation = gameObject.transform.rotation;
-        }
-        else if (buttonBehavior.aAGunSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
-        {
-            GameObject spawnedAAGun = Instantiate(aAGun, transform.position, Quaternion.identity);
-            spawnedAAGun.transform.parent = gameObject.transform;
-            spawnedAAGun.transform.rotation = gameObject.transform.rotation;
-        }
-        else if (buttonBehavior.terraformerSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
-        {
-            GameObject spawnedTerraformer = Instantiate(terraformer, transform.position, Quaternion.identity);
-            spawnedTerraformer.transform.parent = gameObject.transform;
-            spawnedTerraformer.transform.rotation = gameObject.transform.rotation;
-        }
-        else if (buttonBehavior.soldierSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
-        {
-            GameObject spawnedSoldier = Instantiate(soldier, transform.position, Quaternion.identity);
-            spawnedSoldier.transform.parent = gameObject.transform;
-            spawnedSoldier.transform.rotation = gameObject.transform.rotation;
-        }
-        else if (buttonBehavior.shipSelected && gameObject.transform.GetChild(0).tag != "Hazard" && gameObject.transform.childCount < 2)
-        {
-            GameObject spawnedShip = Instantiate(ship, transform.position, Quaternion.identity);
-            spawnedShip.transform.parent = gameObject.transform;
-        }
-
 
     }
 
